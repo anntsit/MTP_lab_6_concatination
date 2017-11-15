c135179070bfb850032917539d8cd600646e3291
anntsit <anyatsit98@ukr.net>
2017-11-15 13:49:35 +0200

    add tests

--- src/test/java/ConcatenationTest.java
+++ src/test/java/ConcatenationTest.java
@@ -1,2 +1,43 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import concatenation.*;
+
+import java.io.IOException;
+
 public class ConcatenationTest {
+    private ConcatenationWithHashSet concatenation;
+
+    @Before
+    public void init() throws Exception {
+        concatenation = new ConcatenationWithHashSet();
+        concatenation.getWordsFromFile("test.txt");
+        concatenation.findConcatenatedWords();
+    }
+
+    @Test
+    public void findConcatenatedWordsTrieTest() throws IOException {
+        String check = "ratcatdogcat";
+        assertEquals(concatenation.concatenatedWords.get(0), check);
+    }
+
+
+    @Test
+    public void getFirstLongestConcatenatedWordTest() {
+        String check = "ratcatdogcat";
+        assertEquals(concatenation.getFirstLongestConcatenatedWord(), check);
+    }
+
+    @Test
+    public void getSecondLongestConcatenatedWordTest() {
+        String check = "catsdogcats";
+        assertEquals(concatenation.getSecondLongestConcatenatedWord(), check);
+    }
+
+    @Test
+    public void getNumberOfConcatenatedWordsTest() {
+        int check = 3;
+        assertEquals(concatenation.getNumberOfConcatenatedWords(), check);
+    }
 }
